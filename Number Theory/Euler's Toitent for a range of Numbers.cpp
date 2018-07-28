83b411c107b8043fe0412080a844d872b43d9d4f
Mohammad Sabik Irbaz <35444388+msi1427@users.noreply.github.com>
2018-07-28 12:39:36 +0600

    Update and rename Euler's Toitent.cpp to Euler's Toitent for a range of Numbers.cpp

--- Number Theory/Euler's Toitent for a range of Numbers.cpp
+++ Number Theory/Euler's Toitent for a range of Numbers.cpp
@@ -24,34 +24,3 @@ int main()
     phi_seive();
     for(long long i=1;i<=99;i++) cout<<i<<" " << phi[i]<< endl;
 }
-///// Actual for any 
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-ll phi(ll n)
-{
-    ll res=0;
-    if (n==1) return 1;
-    res=n;
-    if (n%2==0)
-    {
-        res-=res/2;
-        while (n%2==0) n/=2;
-    }
-    for (ll j=3; j*j<=n; j+=2)
-    {
-        if (n%j==0)
-        {
-            res-=res/j;
-            while (n%j==0) n/=j;
-        }
-    }
-    if (n>1) res-=res/n;
-    return res;
-}
-ll N;
-int main()
-{
-    cin >> N;
-    cout << phi(N) << endl;
-}
