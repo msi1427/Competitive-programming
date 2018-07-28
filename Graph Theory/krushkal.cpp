f7b93640b3a6c167617af80df72b14ce9db4ef6d
Mohammad Sabik Irbaz <35444388+msi1427@users.noreply.github.com>
2018-07-28 13:02:47 +0600

    Create krushkal.cpp

---
+++ Graph Theory/krushkal.cpp
@@ -0,0 +1,55 @@
+#define MAX 6006
+
+ll T,n,m,u,v,w,cnt;
+vector< pair<ll,pll> >ad;
+ll pr[212];
+
+ll FIND(ll r)
+{
+    return (pr[r]==r)?r:FIND(pr[r]);
+}
+
+ll krushkal()
+{
+    sort(all(ad));
+    FLR(i,212) pr[i]=i;
+    ll sum=0,cnt=0,id=-1;
+    FLR(i,sz(ad))
+    {
+        ll xx=ad[i].ss.ff;
+        ll yy=ad[i].ss.ss;
+        ll zz=ad[i].ff;
+        if(FIND(xx)!=FIND(yy))
+        {
+            pr[FIND(xx)]=FIND(yy);
+            cnt++;
+            sum+=zz;
+        }
+        else id=i;
+    }
+    if(id!=-1) ad.erase(ad.begin()+id);
+    return (cnt==n-1)?sum:-1;
+}
+
+int main()
+{
+    scanf("%lld",&T);
+    repl(tc,1,T)
+    {
+        ad.clear();
+        printf("Case %lld:\n",tc);
+        scanf("%lld %lld",&n,&m);
+        FLR(i,m)
+        {
+            cnt=0;
+            scanf("%lld %lld %lld",&u,&v,&w);
+            ad.pb(MP(w,MP(min(u,v),max(u,v))));
+            if(i<n-1)
+            {
+                printf("-1\n");
+                continue;
+            }
+            cout << krushkal() << "\n";
+        }
+    }
+}
