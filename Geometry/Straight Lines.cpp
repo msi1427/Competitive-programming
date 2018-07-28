d49ebc8cc89ffb503a3b2723e9e38e630f461a93
Mohammad Sabik Irbaz <35444388+msi1427@users.noreply.github.com>
2018-07-28 13:12:11 +0600

    Create Straight Lines.cpp

---
+++ Geometry/Straight Lines.cpp
@@ -0,0 +1,9 @@
+i) equation of a line using using 2 points:
+(y-y1)=m(x-x1)
+m=(y2-y1)/(x2-x1)
+
+ii)
+ll collinear(ll x1,ll y1,ll x2,ll y2,ll x3,ll y3)
+{
+    return x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2);
+}
