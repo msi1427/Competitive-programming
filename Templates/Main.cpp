dd6793ce01046d5f1cadacfcabaf82e0549b6bf6
Mohammad Sabik Irbaz <35444388+msi1427@users.noreply.github.com>
2019-12-18 11:08:05 +0600

    Update Main.cpp

--- Templates/Main.cpp
+++ Templates/Main.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
 using namespace __gnu_pbds;
@@ -12,9 +12,9 @@ using namespace __gnu_pbds;
 #define LD           long double
 #define FOR(i,n)     for(int i=0;i<n;i++)
 #define FLR(i,n)     for(ll i=0;i<n;i++)
-#define RFOR(i,n)   for(int i=n-1;i>=0;i--)
+#define RFOR(i,n)    for(int i=n-1;i>=0;i--)
 #define RFLR(i,n)    for(ll i=n-1;i>=0;i--)
-#define rep(i,a,b)    for(int i=a;i<=b;i++)
+#define rep(i,a,b)   for(int i=a;i<=b;i++)
 #define repl(i,a,b)  for(ll i=a;i<=b;i++)
 #define casep(z)     cout << "Case " << z << ": ";
 #define Fast         ios_base::sync_with_stdio(0);cin.tie(0);
@@ -24,10 +24,12 @@ using namespace __gnu_pbds;
 #define RSORT(a,n)   sort(a,a+n,greater<ll>())
 #define pii          pair<int,int>
 #define pll          pair<ll,ll>
+#define tll          tuple<ll,ll,ll>
 #define VSORT(v)     sort(all(v))
-#define VRSORT(v)    sort(all(v),greater<ll>())
+#define VRSORT(v)    sort(v.rbegin(),v.rend())
 #define pb           push_back
-#define pf            push_front
+#define eb           emplace_back
+#define pf           push_front
 #define pob          pop_back()
 #define pof          pop_front()
 #define ff           first
@@ -35,20 +37,20 @@ using namespace __gnu_pbds;
 #define tt           true
 #define fs           false
 #define MP           make_pair
-#define MEM(x,n)      memset((x),n,sizeof(x))
+#define MEM(x,n)     memset((x),n,sizeof(x))
 #define bsrch(a,n,x) binary_search(a,a+n,x)
 #define vi           vector<int>
 #define vll          vector<ll>
-#define PI          acos(-1.0)
+#define PI           acos(-1.0)
 #define LB(a,x)      (lower_bound(all(a),x)-a.begin()) //  first element in the range [first,last) which does not compare less than val.
 #define UB(a,x)      (upper_bound(all(a),x)-a.begin()) //  first element in the range [first,last) which compares greater than val.
 #define PQ           priority_queue
-#define heap       PQ< ll , vll , greater < ll > >
+#define heap         PQ< ll , vector<ll> , greater < ll > >
 #define REV(x)       reverse(all(x))
 #define mpii         map<int,int>
 #define mpll         map<ll,ll>
 #define cig          cin.ignore()
-#define INF          (ll)1e10
+#define INF          (ll)1e18
 #define EPS          1e-9
 #define prec(n)      fixed << setprecision(n)
 #define lb           lower_bound
@@ -58,10 +60,11 @@ using namespace __gnu_pbds;
 #define sc           scanf
 #define pt           printf
 #define np           next_permutation
-#define IT(it,x)     for(it=x.begin();it!=x.end();it++)
+#define IT(it,x)     for(auto it=x.begin();it!=x.end();it++)
 #define sci(x)       scanf("%d",&x);
 #define scl(x)       scanf("%lld",&x);
 #define scd(x)       scanf("%lf",&x);
+#define scs(x)       scanf("%s",x);
 
 #define min3(a,b,c)     min(a,min(b,c))
 #define max3(a,b,c)     max(a,max(b,c))
@@ -69,6 +72,7 @@ using namespace __gnu_pbds;
 #define max4(a,b,c,d)   max(max(a,b),max(c,d))
 #define max5(a,b,c,d,e) max(max3(a,b,c),max(d,e))
 #define min5(a,b,c,d,e) min(min3(a,b,c),min(d,e))
+#define EulerGamma      0.57721566490153286060651209
 
 typedef
 tree<
@@ -101,6 +105,7 @@ ll lcm(ll x,ll y)
 ll BMod(ll B,ll P,ll M)
 {
     ll R=1;
+    B=B%M;
     while(P)
     {
         if(P&1)
@@ -114,6 +119,7 @@ ll BMod(ll B,ll P,ll M)
 bool check(ll n,ll pos) {return (n&(1<<pos));}
 ll SET(ll n,ll pos){return (n|(1<<pos));}
 ll RESET(ll n,ll pos) {return(n&~(1<<pos));}
+mt19937 rng32(chrono::steady_clock::now().time_since_epoch().count());
 
 #define MOD 1000000007
 #define MAX 1000005
@@ -123,4 +129,5 @@ int main()
     Fast
 }
 /*
+
 */
