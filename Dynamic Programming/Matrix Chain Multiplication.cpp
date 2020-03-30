de29db922a1e266ca4309aa33daec57fd9ddb521
Mohammad Sabik Irbaz <35444388+msi1427@users.noreply.github.com>
2020-03-30 16:58:35 +0600

    Update Matrix Chain Multiplication.cpp

--- Dynamic Programming/Matrix Chain Multiplication.cpp
+++ Dynamic Programming/Matrix Chain Multiplication.cpp
@@ -1,40 +1,201 @@
 #include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 
-#define ff first
-#define ss second
+/* msi1427 */
 
-int n,m;
-int dp[105][105];
-vector< int > mat;
+#define ll           long long
+#define ull          long long unsigned
+#define LD           long double
+#define FOR(i,n)     for(int i=0;i<n;i++)
+#define FLR(i,n)     for(ll i=0;i<n;i++)
+#define RFOR(i,n)    for(int i=n-1;i>=0;i--)
+#define RFLR(i,n)    for(ll i=n-1;i>=0;i--)
+#define rep(i,a,b)   for(int i=a;i<=b;i++)
+#define repl(i,a,b)  for(ll i=a;i<=b;i++)
+#define casep(z)     cout << "Case " << z << ": ";
+#define Fast         ios_base::sync_with_stdio(0);cin.tie(0);
+#define sz(a)        a.size()
+#define all(x)       x.begin(),x.end()
+#define SORT(a,n)    sort(a,a+n)
+#define RSORT(a,n)   sort(a,a+n,greater<ll>())
+#define pii          pair<int,int>
+#define pll          pair<ll,ll>
+#define tll          tuple<ll,ll,ll>
+#define VSORT(v)     sort(all(v))
+#define VRSORT(v)    sort(v.rbegin(),v.rend())
+#define pb           push_back
+#define eb           emplace_back
+#define pf           push_front
+#define pob          pop_back()
+#define pof          pop_front()
+#define ff           first
+#define ss           second
+#define tt           true
+#define fs           false
+#define MP           make_pair
+#define MEM(x,n)     memset((x),n,sizeof(x))
+#define bsrch(a,n,x) binary_search(a,a+n,x)
+#define vi           vector<int>
+#define vll          vector<ll>
+#define PI           acos(-1.0)
+#define LB(a,x)      (lower_bound(all(a),x)-a.begin()) //  first element in the range [first,last) which does not compare less than val.
+#define UB(a,x)      (upper_bound(all(a),x)-a.begin()) //  first element in the range [first,last) which compares greater than val.
+#define PQ           priority_queue
+#define heap         PQ< ll , vector<ll> , greater < ll > >
+#define REV(x)       reverse(all(x))
+#define mpii         map<int,int>
+#define mpll         map<ll,ll>
+#define cig          cin.ignore()
+#define INF          (ll)1e18
+#define EPS          1e-9
+#define prec(n)      fixed << setprecision(n)
+#define lb           lower_bound
+#define ub           upper_bound
+#define fin          freopen("in.txt","r",stdin)
+#define fout         freopen("out.txt","w",stdout)
+#define sc           scanf
+#define pt           printf
+#define np           next_permutation
+#define IT(it,x)     for(auto it=x.begin();it!=x.end();it++)
+#define sci(x)       scanf("%d",&x);
+#define scl(x)       scanf("%lld",&x);
+#define scd(x)       scanf("%lf",&x);
+#define scs(x)       scanf("%s",x);
 
-int call(int x,int y){
-    if(x+1>=y) return 0;
-    if(dp[x][y]!=-1) return dp[x][y];
-    int mn = INT_MAX;
-    for(int i=x+1;i<=y-1;i++){
-        int cc = call(x,i)+mat[x]*mat[i]*mat[y]+call(i,y);
-        mn = min(mn,cc);
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(min(a,b),min(c,d))
+#define max4(a,b,c,d)   max(max(a,b),max(c,d))
+#define max5(a,b,c,d,e) max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e) min(min3(a,b,c),min(d,e))
+#define EulerGamma      0.57721566490153286060651209
+
+typedef
+tree<
+  ll,
+  null_type,
+  less<ll>,
+  rb_tree_tag,
+  tree_order_statistics_node_update>
+ordered_set;
+
+ll dx[] = {1,-1,0,0};
+ll dy[] = {0,0,1,-1};
+
+ll ceiling(ll x,ll y)
+{
+    if(x%y) return ((x/y)+1);
+    else return (x/y);
+}
+
+ll gcd(ll x,ll y)
+{
+    return ((y==0)?x:gcd(y,x%y));
+}
+
+ll lcm(ll x,ll y)
+{
+    return ((x*y)/gcd(x,y));
+}
+
+ll BMod(ll B,ll P,ll M)
+{
+    ll R=1;
+    B=B%M;
+    while(P)
+    {
+        if(P&1)
+            R=(R*B)%M;
+        P >>= 1;
+        B=(B*B)%M;
     }
+    return R;
+}
+
+bool check(ll n,ll pos) {return (n&(1<<pos));}
+ll SET(ll n,ll pos){return (n|(1<<pos));}
+ll RESET(ll n,ll pos) {return(n&~(1<<pos));}
+mt19937 rng32(chrono::steady_clock::now().time_since_epoch().count());
+
+#define MOD 1000000007
+#define MAX 1000005
+
+ll n,dp[15][15],pos[15][15];
+vector<pll>mat;
+
+ll call(ll x,ll y){
+    if(x==y) return 0;
+    if(dp[x][y]!=-1) return dp[x][y];
+    ll mn = INF;
+    ll idx = -1;
+    for(ll i=x;i<y;i++){
+        ll cc = call(x,i)+mat[x].ff*mat[i].ss*mat[y].ss+call(i+1,y);
+        mn = min(mn,cc);
+        if(mn>=cc) {
+            mn=cc;
+            idx=i;
+        }
+    }
+    pos[x][y]=idx;
     return dp[x][y]=mn;
 }
 
-int main(){
-    while(cin>>n){
-    mat.resize(n);
-    memset(dp,-1,sizeof(dp));
-    for(int i=0;i<n;i++){
-        cin>>mat[i];
+string STOI(ll x){
+    x++;
+    string s;
+    while(x){
+        s.pb((x%10)+'0');
+        x/=10;
     }
-    cout << call(0,n-1) << "\n";
+    REV(s);
+    return s;
+}
+
+string path(ll x,ll y){
+    if(x==y){
+        string cur;
+        cur.pb('A');
+        cur+=STOI(x);
+        return cur;
+    }
+    string cur;
+    cur.pb('(');
+    cur+=path(x,pos[x][y]);
+    cur.pb(' ');
+    cur.pb('x');
+    cur.pb(' ');
+    cur+=path(pos[x][y]+1,y);
+    cur.pb(')');
+    return cur;
+}
+
+int main()
+{
+    Fast
+    repl(tc,1,MAX){
+        cin>>n;
+        if(n==0) return 0;
+        mat.resize(n);
+        MEM(dp,-1);
+        MEM(pos,-1);
+        FLR(i,n) cin>>mat[i].ff>>mat[i].ss;
+        call(0,n-1);
+        string ans = path(0,n-1);
+        cout << "Case " << tc << ": " << ans << "\n";
     }
 }
 /*
-5
-5
-4
-6
-2
-7
+3
+1 5
+5 20
+20 1
+3
+5 10
+10 20
+20 35
+0
 */
